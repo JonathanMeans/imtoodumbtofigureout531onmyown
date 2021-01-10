e17fe0b70a7e463d0b76ea4ae02bc22f619c9d34
JETM319 <jetmeans@gmail.com>
2021-01-10 10:44:27 -0500

    Failing FT up to getting deadlift numbers

--- functional_tests/test_start_workout.py
+++ functional_tests/test_start_workout.py
@@ -1,5 +1,22 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class SimpleWorkoutTest(StaticLiveServerTestCase):
-    pass
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_workout_and_see_plan(self):
+        # Shantae goes to her training website to try out the 531 for beginners workout
+        self.browser.get(self.live_server_url)
+
+        # She is asked to enter her training max for deadlift
+        inputbox = self.browser.find_element_by_id("id_deadlift_tmax_input")
+
+        # She enters her impressive starting point
+        inputbox.send_keys("425")
+        inputbox.send_keys(Keys.ENTER)
