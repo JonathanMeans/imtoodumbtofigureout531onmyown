f0ba73a0d9c7d373acb9244af1a5c01ff7ba8157
JETM319 <jetmeans@gmail.com>
2021-01-16 11:33:39 -0500

    Add fabfile to automate deployment

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import cd, env, run
+from fabric.contrib.files import exists
+from fabric.operations import local
+
+REPO_URL = "git@github.com:JonathanMeans/imtoodumbtofigureout531onmyown.git"
+
+
+def _get_latest_source():
+    if exists(".git"):
+        run("git fetch")
+    else:
+        run(f"git clone {REPO_URL} .")
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f"git reset --hard {current_commit}")
+
+
+def _update_pipenv():
+    run("pipenv update")
+
+
+def _update_static_files():
+    run("pipenv run python manage.py collectstatic --noinput")
+
+
+def deploy():
+    site_folder = f"/home/sites/{env.host}"
+    run(f"mkdir -p {site_folder}")
+    with cd(site_folder):
+        _get_latest_source()
+        _update_pipenv()
+        _update_static_files()
