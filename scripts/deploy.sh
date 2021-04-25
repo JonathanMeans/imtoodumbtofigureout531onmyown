fce98e14c54bcf6dfeb2320835a9a30b09b46fc7
JETM319 <jetmeans@gmail.com>
2021-04-25 15:22:24 -0400

    Try to deploy to DO from Travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+ssh -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
+  cd /home/sites/www.imtoodumbtofigureout531onmyown-staging.com/
+  docker stop jmeans319/531
+  docker rm jmeans319/531
+  docker image prune -a -f
+  docker pull jmeans319/531
+  sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
+ENDSSH
