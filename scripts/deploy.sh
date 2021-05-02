c4f7b1ee13b32531d602e919d42a480f4816c5b2
JETM319 <jetmeans@gmail.com>
2021-05-02 09:37:04 -0400

    Sudo for docker?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
   cd /home/sites/www.imtoodumbtofigureout531onmyown-staging.com/
-  docker stop jmeans319/531
-  docker rm jmeans319/531
-  docker image prune -a -f
+  sudo docker stop jmeans319/531
+  sudo docker rm jmeans319/531
+  sudo docker image prune -a -f
   sudo git pull
-  docker build .
+  sudo docker build .
   sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
 ENDSSH
