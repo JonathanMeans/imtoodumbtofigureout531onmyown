408a35cbbbf6767640a61efa0823b2c9cda2a572
JETM319 <jetmeans@gmail.com>
2021-05-02 09:22:07 -0400

    Try manually rebuilding docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
   docker stop jmeans319/531
   docker rm jmeans319/531
   docker image prune -a -f
-  docker pull jmeans319/531
+  docker build .
   sudo git pull
   sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
 ENDSSH
