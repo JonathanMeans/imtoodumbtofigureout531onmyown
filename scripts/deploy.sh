a72251f1a311871e78d967fdf40b0c5f195b9e77
JETM319 <jetmeans@gmail.com>
2021-05-19 20:36:30 -0400

    Hopefully fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,6 @@ ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
   sudo docker rm jmeans319/531
   sudo docker image prune -a -f
   sudo docker pull jmeans319/531
+  git pull
   sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
 ENDSSH
