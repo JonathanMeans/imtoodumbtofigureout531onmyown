4b16dafc024e036c171532a231bb52735bf7d60f
JETM319 <jetmeans@gmail.com>
2021-05-22 07:35:44 -0400

    Simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,7 @@ set -e
 
 ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
   cd /home/sites/www.imtoodumbtofigureout531onmyown-staging.com/
-  sudo docker stop jmeans319/531
-  sudo docker rm jmeans319/531
-  sudo docker image prune -a -f
-  sudo docker pull jmeans319/531
+  sudo systemctl stop gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
   sudo git pull
-  python3 manage.py collectstatic --noinput
   sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
 ENDSSH
