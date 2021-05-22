44bd8b8458685fb08895f171c94b3b79f0dee393
JETM319 <jetmeans@gmail.com>
2021-05-22 13:02:22 -0400

    Remove testing text and force reset before pull to prevent stupid blockers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ SITE_NAME=$1
 ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
   cd /home/sites/www.${SITE_NAME}/
   sudo systemctl stop gunicorn-${SITE_NAME}.service
+  sudo git reset --hard HEAD
   sudo git pull
   sudo env/bin/python manage.py collectstatic --no-input
   sudo systemctl restart gunicorn-${SITE_NAME}.service
