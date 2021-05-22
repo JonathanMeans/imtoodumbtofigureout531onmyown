010a45395fa677a6d18353076a2c8ff4bac123da
JETM319 <jetmeans@gmail.com>
2021-05-22 12:50:38 -0400

    Consolidate deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+SITE_NAME=$1
 
 ssh -v -o StrictHostKeyChecking=no travis@206.189.200.76 << ENDSSH
-  cd /home/sites/www.imtoodumbtofigureout531onmyown-staging.com/
-  sudo systemctl stop gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
+  cd /home/sites/www.${SITE_NAME}/
+  sudo systemctl stop gunicorn-${SITE_NAME}.service
   sudo git pull
   sudo env/bin/python manage.py collectstatic --no-input
-  sudo systemctl restart gunicorn-imtoodumbtofigureout531onmyown-staging.com.service
+  sudo systemctl restart gunicorn-${SITE_NAME}.service
 ENDSSH
